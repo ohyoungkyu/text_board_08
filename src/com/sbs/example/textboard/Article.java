1c496bb24f029347ff4a41314334ca41cff96be4
ohyoungkyu <come8908@gmail.com>
2021-08-19 19:33:19 +0900

    게시글 작성 리스팅 및 JDBC Test

--- src/com/sbs/example/textboard/Article.java
+++ src/com/sbs/example/textboard/Article.java
@@ -1,7 +1,17 @@
 package com.sbs.example.textboard;
 
-public class Article {
+public class Article extends Object {
 	public int id;
 	public String title;
 	public String body;
+	
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+	
+	public String toString() {
+		return "Article [id=" + id + ", title=" + title + ", body=" + body + "]";
+	}
 }
--- src/com/sbs/example/textboard/Main.java
+++ src/com/sbs/example/textboard/Main.java
@@ -5,47 +5,53 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		List<Article> articles = new ArrayList<>();
-		int lastArticleid = 0;
-		
-		while(true) {
+
+		int lastArticleId = 0;
+
+		while (true) {
 			System.out.printf("명령어) ");
 			String command = sc.nextLine().trim();
-			
-			if(command.length()==0) {
-				continue;
+
+			if (command.equals("article write")) {
+				int id = lastArticleId + 1;
+				String title;
+				String body;
+
+				System.out.println("== 게시글 생성 ==");
+				System.out.printf("제목 : ");
+				title = sc.nextLine();
+				System.out.printf("내용 : ");
+				body = sc.nextLine();
+
+				Article article = new Article(id, title, body);
+				articles.add(article);
+				lastArticleId++;
+
+				System.out.println(article);
+
+			} else if (command.equals("article list")) {
+				System.out.println("== 게시글 리스트 ==");
+
+				if (articles.size() == 0) {
+					System.out.println("게시물이 존재하지 않습니다.");
+					continue;
+				}
+				System.out.println("번호  /  제목");
+
+				for (Article article : articles) {
+					System.out.printf("%d    /   %s\n", article.id, article.title);
+				}
+
 			}
-			
-			if(command.equals("system exit")) {
+
+			else if (command.equals("system exit")) {
+				System.out.println("== 프로그램 종료 ==");
 				break;
 			}
-			
-			if(command.equals("article write")) {
-				int id = lastArticleid + 1;
-				lastArticleid = id;
-				
-				System.out.printf("제목 : ");
-				String title = sc.nextLine();
-				System.out.printf("내용 : ");
-				String body = sc.nextLine();
-				
-				System.out.printf("%d번 글이 생성되었습니다.\n",id);
-			}
-			
-			else if(command.equals("article list")) {
-				System.out.printf("게시물이 존재하지 않습니다.\n");
-			}
-			
-			else {
-				System.out.printf("존재하지 않는 명령어입니다.\n");
-			}
 		}
-		
-		System.out.println("프로그램을 종료합니다.");
 	}
-
 }
