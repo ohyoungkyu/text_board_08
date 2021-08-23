2be8bd0ca8116175d9c0ab2750ef30a9d2085c91
ohyoungkyu <come8908@gmail.com>
2021-08-23 18:03:57 +0900

    SLEECT TEST및 article list 수정, modify 기능추가

--- src/com/sbs/example/textboard/Article.java
+++ src/com/sbs/example/textboard/Article.java
@@ -2,8 +2,21 @@ package com.sbs.example.textboard;
 
 public class Article extends Object {
 	public int id;
+	public String regDate;
+	public String updateDate;
 	public String title;
 	public String body;
+
+	
+	public Article(int id, String regDate, String updateDate, String title, String body) {
+		super();
+		this.id = id;
+		this.regDate = regDate;
+		this.updateDate = updateDate;
+		this.title = title;
+		this.body = body;
+
+	}
 	
 	public Article(int id, String title, String body) {
 		this.id = id;
@@ -12,6 +25,7 @@ public class Article extends Object {
 	}
 	
 	public String toString() {
-		return "Article [id=" + id + ", title=" + title + ", body=" + body + "]";
+		return "Article [id=" + id + ", regDate=" + regDate + ", updateDate=" + updateDate + ", title=" + title
+				+ ", body=" + body + "]";
 	}
 }
